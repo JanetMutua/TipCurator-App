8482eef84aae2f299d3fc405464fd84aa4197310
Janet <janetmutua04@gmail.com>
2023-02-27 20:23:42 +0300

    View binding

--- app/src/main/java/com/example/tip_curator/MainActivity.kt
+++ app/src/main/java/com/example/tip_curator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.tip_curator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tip_curator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    Top level variable to be used across different methods in this class
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
